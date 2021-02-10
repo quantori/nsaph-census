0a67bf4abb5aa3a37ca5d00935c2db5a200670c6
mbsabath <bsabath1@gmail.com>
2021-02-10 10:18:31 -0500

    data assembly

--- get_census/assemble_data.py
+++ get_census/assemble_data.py
@@ -1,26 +1,41 @@
 from .census_info import census_years
-from .query import get_census_data
+from .query import get_census_data, clean_acs_vars
 import pandas as pd
 import yaml
 
 class DataPlan:
     """
-    a class de
+    a class containing information on how to create a desired set of census data.
+    Members:
+        geometry: which census geography this plan is for
+        years: The list of years that the data should be queried for
+        plan: A dictionary with keys of years, storing lists of VariableDef objects defining the
+            variables to be calculated for that year. Created from a yaml file. LINK TO YAML INSTRUCTIONS HERE
+        data: A pandas data frame created based on the defined data plan. only exists after the DataPlan.assemble_data()
+        method is called.
     """
     def __init__(self, yaml_path, geometry, years=census_years()):
         """
         initialize a DataPlan object from a get_census yaml document
         :param yaml_path: path to a yaml file
+        :param geometry: which census geography this plan is for
+        :param years: The list of years to query data from. The census_years() function can
+            calculate which years in your timeframe of interest can be queried for the decennial and
+            5 year acs data. Note that this may not apply for the ACS1 or other data. That function may be
+            updated in the future, but for now creating lists of years besides the defaults is left as an exercise
+            for the interested reader.
         """
         self.geometry = geometry
         self.years = years
+        if type(self.years) is int: self.years = [self.years]
+
         self.plan = dict()
-        self.yaml_to_dict(yaml_path, years)
+        self.yaml_to_dict(yaml_path)
 
         self.data = None
 
 
-    def yaml_to_dict(self, yaml_path, years):
+    def yaml_to_dict(self, yaml_path):
         """
         Convert a yaml file detailing how to get census variables in to a dictionary. Handles
         the issue of forward counting years to make future code readable.
@@ -28,6 +43,7 @@ class DataPlan:
         INSERT LINK TO CENSUS README TO GUIDE HOW TO WRITE THE YAML
 
         :param yaml_path:
+        :param years:
         :return: dictionary
         """
 
@@ -36,13 +52,36 @@ class DataPlan:
         with open(yaml_path) as f:
             yaml_dict = yaml.load(f, Loader=yaml.FullLoader)
 
-        for year in years:
+        for year in self.years:
             self.plan[year] = list()
             for varname in yaml_dict.keys():
                 plan_year = find_year(year, list(yaml_dict[varname].keys()))
                 self.plan[year].append(VariableDef(varname, yaml_dict[varname][plan_year]))
 
+    def assemble_data(self):
+        """
+        Create a data frame for each geoid each year, for
+        :return:
+        """
 
+        self.data = None # Clear in case this has been run previously
+
+        for year in self.years:
+            year_data = None
+            for var_def in self.plan[year]:
+                print(year, var_def.name)
+                var_data = var_def.calculate_var(year, self.geometry)
+                join_vars = list(set(var_data.columns).difference([var_def.name]))
+
+                if year_data is None:
+                    year_data = pd.DataFrame(columns=join_vars)
+
+                year_data = pd.merge(year_data, var_data, how="outer", on=join_vars)
+
+            if self.data is None:
+                self.data = year_data
+            else:
+                self.data = self.data.append(year_data, ignore_index=True)
 
 
 
@@ -68,6 +107,9 @@ class VariableDef:
             self.has_den = True
             self.den = den
 
+        if "acs" in self.dataset: self.make_acs_vars()
+
+
     def __init__(self, name, var_dict):
 
         self.name = name
@@ -84,6 +126,12 @@ class VariableDef:
         else:
             self.has_den = False
             self.den = []
+
+        if "acs" in self.dataset: self.make_acs_vars()
+
+    def make_acs_vars(self):
+        clean_acs_vars(self.num)
+        if self.has_den: clean_acs_vars(self.den)
 
     def get_vars(self):
         """
@@ -165,7 +213,7 @@ def find_year(year, year_list):
     :return: first year greater than or equal to "year" in "year_list"
     """
 
-    year_list.sort()  #Should be sorted, but just in case
+    year_list.sort()  # Should be sorted, but just in case
     for i in year_list:
         if i >= year:
             return i
