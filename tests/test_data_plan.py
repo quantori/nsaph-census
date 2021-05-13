9ab8df4733478d689c2d3d0bedfb8743025403f5
mbsabath <bsabath1@gmail.com>
2021-05-13 14:10:55 -0400

    more tests

--- tests/test_data_plan.py
+++ tests/test_data_plan.py
@@ -1,11 +1,13 @@
 import unittest
-
 import get_census
 
 
-class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)
+class DataPlanTest(unittest.TestCase):
+    def test_load_yaml(self):
+        plan = get_census.DataPlan("data_cache/test_vars.yml", geometry= "state", years=[x for x in range(2009, 2012)])
+        self.assertEqual(list(plan.plan.keys()), [2009, 2010, 2011])
+        self.assertEqual(len(plan.plan[2009]), 3)
+
 
 
 if __name__ == '__main__':
